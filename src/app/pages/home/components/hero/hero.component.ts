76d3783d4fcc5db499b2aa2df17e984fff047375
Borysenko Dmytro <dbo@binotel.com>
2026-02-11 22:15:47 +0200

    fix

--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <section class="hero-block">
       <h1 i18n="@@home.hero.title" class="title">A full-funnel digital marketing agency</h1>
-      <p i18n="@@home..hero.description" class="description">You can always be a better, reach higher results, and
+      <p i18n="@@home.hero.description" class="description">You can always be a better, reach higher results, and
         exceed expectations with our advanced AI tools and deep expertise across the digital marketing funnel. We
         empower clients to enhance revenue, boost brand visibility and loyalty, and grow their customer base
         effectively</p>
